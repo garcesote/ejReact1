195243cef8653ec6a346c00558d9effe12ca67f4
Iñigo Garcia Ugarte <garcia.127407@e.unavarra.es>
2023-02-27 19:56:00 +0100

    estado en los botones

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,10 +1,37 @@
-import { useState } from "react";
+import { useState, useRef, useEffect} from "react";
+import ReactDOM from 'react-dom';
+
+const InfoModal = (props) => {
+    const [open, setOpen] = useState(true);
+
+    const clickHandler = () => {
+        setOpen(false);
+    }
+
+    if (open) {
+        return (
+            <>
+                <h1>{props.titulo}</h1>
+                <p>{props.mensaje}</p>
+                <button onClick={clickHandler}>CERRAR</button>
+            </>
+        )
+    }
+
+    return null;
+}
 
 const Layout = () => {
 
     const [inputA, setInputA] = useState('');
     const [inputB, setInputB] = useState('');
     const [result, setResult] = useState('');
+    const [opr, setOperation] = useState('');
+
+    const sumaRef = useRef();
+    const restaRef = useRef();
+    const multRef = useRef();
+    const divRef = useRef();
 
     const inputAHandler = (event) => {
         setInputA(event.target.value);
@@ -19,64 +46,78 @@ const Layout = () => {
         var a = parseInt(inputA);
         var b = parseInt(inputB);
 
+        console.log(opr);
+
+        if(opr=='suma'){
+            console.log(opr);
+            sumaRef.current.className = "btn btn-outline-primary"
+        }else if(opr=='resta'){
+            console.log(opr);
+            restaRef.current.className = "btn btn-outline-primary"
+        }else if(opr=='multiplicacion'){
+            console.log(opr);
+            multRef.current.className = "btn btn-outline-primary"
+        }else if(opr=='division'){
+            console.log(opr);
+            divRef.current.className = "btn btn-outline-primary"
+        }
+
         switch(operation){
-            case 0: setResult(a+b); break;
-            case 1: setResult(a-b); break;
-            case 2: setResult(a*b); break;
-            case 3: setResult(a/b); break;
-            default: setResult(a+b); break;
+            case 0: setResult(a+b); setOperation('suma'); sumaRef.current.className = "btn btn-outline-primary active" ; break;
+            case 1: setResult(a-b); setOperation('resta'); restaRef.current.className = "btn btn-outline-primary active"; break;
+            case 2: setResult(a*b); setOperation('multiplicacion'); multRef.current.className = "btn btn-outline-primary active"; break;
+            case 3: setResult(a/b); setOperation('division'); divRef.current.className = "btn btn-outline-primary active"; break;
+            default: setResult(a+b); setOperation('suma'); break;
         }
     }
 
     return(
-
-        <div class="justifycontent-center">
-            <form>
-                <div class="container-fluid row">
-                    <div class="col-md-6 border text-center bg-light">
-                        <span class="input-group-text m-4">
-                            <p class="h3">A: </p><input onChange={inputAHandler} type='number' value={inputA} 
-                            placeholder="Introduce first number" class="form-control w-50 m-4 h-5"/>
-                        </span>
+            <div className="justifycontent-center">
+                <form>
+                    <div class="container-fluid row">
+                        <div class="col-md-6 border text-center bg-light">
+                            <span class="input-group-text m-4">
+                                <p class="h3">A: </p><input  onChange={inputAHandler} type='number' value={inputA} 
+                                placeholder="Introduce first number" class="form-control w-50 m-4 h-5"/>
+                            </span>
+                        </div>
+                        <div class="col-md-6 border text-center bg-light">
+                            <span class="input-group-text m-4">
+                                <p class="h3">B: </p> <input onChange={inputBHandler} type='number' value={inputB} 
+                                placeholder="Introduce second number" class="form-control w-50 m-4 h-5"/>
+                            </span>
+                        </div>
                     </div>
-                    <div class="col-md-6 border text-center bg-light">
-                        <span class="input-group-text m-4">
-                            <p class="h3">B: </p> <input onChange={inputBHandler} type='number' value={inputB} 
-                            placeholder="Introduce second number" class="form-control w-50 m-4 h-5"/>
-                        </span>
+                </form>
+
+                <div class="container-fluid row mt-5">
+                    <div class="col-md-3 border text-center bg-light">
+                        <button type='submit' ref={sumaRef} class="btn btn-outline-primary" onClick={() => {
+                            operationHandler(0);
+                        }}>+</button>
+                    </div>
+                    <div class="col-md-3 border text-center bg-light">
+                        <button type='submit' ref={restaRef} class="btn btn-outline-primary" onClick={() => {
+                            operationHandler(1);
+                        }}>-</button>
+                    </div>
+                    <div class="col-md-3 border text-center bg-light">
+                    <button type='submit' ref={multRef} class="btn btn-outline-primary" onClick={() => {
+                            operationHandler(2);
+                        }}>x</button>
+                    </div>
+                    <div class="col-md-3 border text-center bg-light">
+                    <button type='submit' ref={divRef} class="btn btn-outline-primary" onClick={() => {
+                            operationHandler(3);
+                        }}>/</button>
                     </div>
                 </div>
-            </form>
-
-            <div class="container-fluid row mt-5">
-                <div class="col-md-3 border text-center bg-light">
-                    <button type='submit' class="btn btn-outline-primary" onClick={() => {
-                        operationHandler(0);
-                    }}>+</button>
-                </div>
-                <div class="col-md-3 border text-center bg-light">
-                    <button type='submit' class="btn btn-outline-primary" onClick={() => {
-                        operationHandler(1);
-                    }}>-</button>
-                </div>
-                <div class="col-md-3 border text-center bg-light">
-                <button type='submit' class="btn btn-outline-primary" onClick={() => {
-                        operationHandler(2);
-                    }}>x</button>
-                </div>
-                <div class="col-md-3 border text-center bg-light">
-                <button type='submit' class="btn btn-outline-primary" onClick={() => {
-                        operationHandler(3);
-                    }}>/</button>
+                
+                <div class="card-body text-center mt-5 border row">
+                        <div class="card-title border w-50 h3">Result: </div>
+                    <div class="card-title border w-50 h4">{result}</div>
                 </div>
             </div>
-            
-            <div class="card-body text-center mt-5 border row">
-                    <div class="card-title border w-50 h3">Result: </div>
-                   <div class="card-title border w-50 h4">{result}</div>
-            </div>
-        </div>
-        
     )
 }
 
